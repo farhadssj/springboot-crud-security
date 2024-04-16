607eab3e5a7339f9f5c7e3b7dbe08b2fc4c1e9c7
Md. Farhad Hoosain <farhad1173@gmail.com>
2024-04-16 16:55:07 +0600

    SpringBoot Initial project added

---
+++ src/main/java/com/programming/springboot/SpringbootCrudSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.programming.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudSecurityApplication.class, args);
+	}
+
+}
